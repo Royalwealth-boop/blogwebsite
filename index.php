70240eb736dbbbc01782ec5325e6a9897282a9a4
Royalwealth-boop <royalwealth95@gmail.com>
2024-06-12 05:46:06 -0700

    myfirstcommit

---
+++ index.php
@@ -0,0 +1,265 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<img src="images/businesswatch.png" alt="">
+
+<body>  
+    <nav>
+    <ul>
+        <li><a href="index.php">Home</a></li>
+        <li><a href="index.php">Business</a></li>
+        <li><a href="index.php">Investors & Analysis</a></li>
+        <li><a href="index.php">Economy</a></li>
+        <li><a href="index.php">Market</a></li>
+        <li><a href="index.php">Regulators</a></li>
+        <li><a href="index.php">Appointments</a></li>
+        <li><a href="index.php">Rankings & Data</a></li>
+        <li><a href="brands.php">Brands & Awards</a></li>
+    </ul>
+    </nav>
+</body>
+
+<img src="images/accessimage.png" alt="">
+
+
+<section class="headline">
+    <h3>HEADLINE STORIES</span></h3>
+    <div class="container">
+
+<!-- 1ST BOX STARTS HERE -->
+<section class="headline1">
+    <div class="overlay"></div>
+    <div class="content">
+        <h3>MARKETS</h3>
+        <p>Guaranty Trust Holding Company(GTCO)Plc Gains Shareholders Approval for $750 Million Capital Raise</p>  
+    </div>
+</section>
+<!--1st box ends here  -->
+
+<!-- 2nd box starts here -->
+<section class="headline2">
+    <div class="overlay"></div>
+    <div class="content">
+        <h3>APPOINTMENTS</h3>
+        <p>Beta Glass Plc Announces Changes To its Board Of Directors</p>   
+    </div>
+</section>
+<!-- 2nd box ends here -->
+
+<!-- 3rd box starts here -->
+<section class="headline3">
+    <div class="overlay"></div>
+    <div class="content">
+        <h3>MARKETS</h3>
+        <p>Unilever Nigeria Plc Holds Annual General Meeting, Announces Key Resolutions</p>   
+    </div>
+</section>
+<!-- 3rd box ends here -->
+
+<!-- 4th box starts here -->
+<section class="headline4">
+    <div class="overlay"></div>
+    <div class="content">   
+    </div>
+    <!-- 4th box ends here -->
+    </div>
+</section>
+
+<!-- top stories starts here -->
+<section class="topstories">
+    <h3>TOP <span>STORIES</span></h3>
+
+    <div class="container1">
+         <!-- 1st box starts here -->
+        <div class="box">
+            <img src="images/sw1.webp" alt="">
+           </div>
+    
+        <div class="box">
+            <h5>APPOINTMENTS</h5>
+            <h4>Stanbic IBTC Group Announces Retirement Of Chairman Basil Omiyi CON</h4>
+            <p>May 21, 2024</p>
+            <p>Stanbic IBTC Group has officially announced the retirement of Mr. Basil Omiyi CON from his position as Chairman and Non-Executive Director</p>
+      </div>
+        </div>
+        <!-- 1st box ends here -->
+
+        <div class="container2">
+        <!-- 2nd box starts here -->
+        <div class="box">
+            <img src="images/sw2.webp" alt="">
+        </div>
+
+        <div class="box">
+        <h5>MARKETS</h5>
+        <h4>Seplat Energy PLC Announces Exercise Of Vested Shares by Roger Brown</h4>
+            <p>May 21, 2024</p>
+            <p>Seplat Energy PLC announces that Roger Brown has used his right to claim shares given to him between 2014 and 2022 under the LTI Plan.</p>
+        </div>
+        </div>
+
+        <!-- 2nd box ends here -->
+
+        <div class="container3">
+        <!-- 3rd box starts here -->
+        <div class="box">
+            <img src="images/sw3.jpg" alt="">
+        </div>
+
+        <div class="box">
+            <h5>MARKETS</h5>
+            <h4>Debt Management Office Raise Over 390billion Naira in May 2024 Bond Auction</h4>
+            <p>May 21, 2024 </p>
+            <p>Despite the competitive bidding environment, the auction saw 28 successful bids for the 5-Year bond, 39 for the 7-Year bond, and 120 for the 9-Year bond, resulting in a…</p>
+        </div>
+        </div>
+        <!-- 3rd box ends here -->
+
+        <div class="container4">
+        <!-- 4th box starts starts here -->
+        <div class="box">
+            <img src="images/sw4.webp" alt="">
+        </div>
+        
+        <div class="box">
+            <h5>MARKETS</h5>
+            <h4>Key Resolution At The 64TH Annual General Meeting Of Berger Paints Nigeria PLC</h4>
+            <P>May 21, 2024</P>
+            <p>At the 64th Annual General Meeting of Berger Paints Nigeria Plc held on Tuesday, 15 May, 2024, the following resolutions were duly passed.</p>
+        </div>
+        </div>
+        <!-- 4th box ends here -->
+
+        <div class="container5">
+            <!--5th box starts here  -->
+            <div class="box">
+                <img src="images/sw5.jpg" alt="">
+            </div>
+
+            <div class="box">
+                <h5>OPINION</h5>
+                <h4>President Tinubu First Year:The Fraught Stage Of Nigeria's Economy</h4>
+                <p>May 21, 2024</p>
+                <p>Fans of the “Emi lo kan President”, President Bola Ahmed Tinubu, will probably agree that the President has done a great job in his first year.</p>
+            </div>
+        </div>
+        <!-- 5th box ends here -->
+        
+        <div class="container6">
+        <!-- 6th box starts here -->
+        <div class="box">
+                <img src="images/sw6.jpg" alt="">
+            </div>
+             
+            <div class="box">
+                <h5>MARKETS</h5>
+                <h4>Guaranty Trust Holding Company (GTCO) Plc Gains Shareholder Approval For $750 Million Capital Raise</h4>
+                <p>may 11, 2024</p>
+                <p>Shareholders endorsed a substantial dividend payout of N94.179 billion for the year ended December 31, 2023.</p>
+            </div>
+        </div>
+        <!-- 6th box ends here -->
+    
+        <div class="container7">
+        <!-- 7th box starts here -->
+        <div class="box">
+                <img src="images/sw7.jpg" alt="">
+            </div>
+             
+            <div class="box">
+                <h5>MARKETS</h5>
+                <h4>Guaranty Trust Holding Company (GTCO) Plc Gains Shareholder Approval For $750 Million Capital Raise</h4>
+                <p>may 11, 2024</p>
+                <p>Shareholders endorsed a substantial dividend payout of N94.179 billion for the year ended December 31, 2023.</p>
+            </div>
+        </div>
+        <!-- 7th box ends here -->
+    
+</section>
+<!-- services section ends here -->
+
+<!-- top stories ends here -->
+
+<!-- MOST VIEWED STORIES STARTS HERE -->
+
+<section class="most-viewed">
+    <h3>MOST VIEWED STORIES</h3>
+<div class="container">
+  <div class="box">
+    <div class="overlay"></div>
+    <div class="content">
+        <a href="" class="market">MARKETS</a>
+        <p>Gtbank appoints new chairman</p>
+    </div>
+  </div>
+
+  <div class="box">
+    <div class="overlay"></div>
+    <div class="content">
+        <a href="" class="market">APPOINTMENTS</a>
+        <p>Gtbank appoints new chairman</p>
+    </div>
+  </div>
+
+  <div class="box">
+    <div class="overlay"></div>
+    <div class="content">
+        <a href="" class="market">Market</a>
+        <p>Gtbank appoints new chairman</p>
+    </div>
+  </div>
+
+  <div class="box">
+    <div class="overlay"></div>
+    <div class="content">
+        <a href="" class="market">Market</a>
+        <p>Gtbank appoints new chairman</p>
+    </div>
+  </div>
+</div>
+</section>
+<!-- MOST VIEWED STORIES ENDS HERE -->
+
+<section class="mostview">
+    <div class="containerA">
+        <div class="box1">
+            <div class="overlay"></div>
+            <div class="content">
+                <a href="" class="MARKETS">MARKETS</a>
+                <P>Debts Management Office Raise Over 380 Billion in May 2024 Bond Auction</P>
+            </div>
+        </div>
+        
+        <div class="box2">
+            <div class="overlay"></div>
+            <div class="content">
+                <a href="" class="MARKETS">MARKETS</a>
+                <p>Debts Management Office Raise Over 380 million in May 2024 Bond Auction</p>
+            </div>
+        </div>
+
+        <div class="box3">
+            <div class="overlay"></div>
+            <div class="content">
+                <a href="" class="MARKETS">MARKETS</a>
+                <p>Debts Management Office Raise Over 380 million in May 2024 Bond Auction</p>
+            </div>
+             </div>
+
+             <div class="box4">
+            <div class="overlay"></div>
+            <div class="content">
+                <a href="" class="MARKETS">MARKETS</a>
+                <p>Debts Management Office Raise Over 380 million in May 2024 Bond Auction</p>
+            </div>
+             </div>
+    </div>
+
+</section>
+</html>
